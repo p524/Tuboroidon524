78c35f56733e6064205474c1d08309af6b107d0c
gaeeyo <iknnofm0126@gmail.com>
2010-11-02 23:39:13 +0900

    import Debugを削除

--- Tuboroid/src/info/narazaki/android/tuboroid/agent/ThreadEntryListTask.java
+++ Tuboroid/src/info/narazaki/android/tuboroid/agent/ThreadEntryListTask.java
@@ -18,7 +18,6 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-import android.os.Debug;
 import android.util.Log;
 
 abstract public class ThreadEntryListTask {
