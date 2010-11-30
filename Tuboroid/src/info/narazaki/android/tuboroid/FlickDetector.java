f859b41e65a3a5509e4f4b72b0ee00e457d292dc
gaeeyo <iknnofm0126@gmail.com>
2010-11-30 23:55:02 +0900

    バージョンを0.0.17に

--- Tuboroid/src/info/narazaki/android/tuboroid/FlickDetector.java
+++ Tuboroid/src/info/narazaki/android/tuboroid/FlickDetector.java
@@ -1,7 +1,6 @@
 package info.narazaki.android.tuboroid;
 
 import android.app.Activity;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.GestureDetector.OnGestureListener;
 
