937bbeb6bd168558086cc6a574769991293498f7
p524 <pppp524p@yahoo.co.jp>
2011-01-01 03:59:45 +0900

    Revert
    This reverts commit c88c3f96522fee562783d9ca08b2ac0fbd566586.

--- Tuboroid/src/info/narazaki/android/tuboroid/FlickDetector.java
+++ Tuboroid/src/info/narazaki/android/tuboroid/FlickDetector.java
@@ -2,7 +2,6 @@ package info.narazaki.android.tuboroid;
 
 import android.app.Activity;
 import android.view.MotionEvent;
-import android.view.ViewConfiguration;
 import android.view.GestureDetector.OnGestureListener;
 
 public abstract class FlickDetector {
@@ -11,7 +10,6 @@ public abstract class FlickDetector {
 	public abstract static class OnFlickListener implements OnGestureListener {
 		private boolean handled;
 		private Activity activity;
-		private final static int touchSlop = ViewConfiguration.getTouchSlop();
 		
 		public OnFlickListener(Activity a) {
 			activity = a;
@@ -31,17 +29,14 @@ public abstract class FlickDetector {
 		public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX,
 				float velocityY) {
 			if (e1 == null || e2 == null) return false;
-			if (Math.abs(e2.getX() - e1.getX()) > touchSlop) {
-				float x = Math.abs(velocityX);
-				float y = Math.abs(velocityY);
-				if (x > y*3 && x > activity.getResources().getDisplayMetrics().widthPixels) {
-					if (Math.abs(e1.getX()-e2.getX()) > Math.abs(e1.getY()-e2.getY())) {
-						handled = (velocityX > 0 ? onFlickRight() : onFlickLeft());
-						if (handled) {
-							e2.setAction(MotionEvent.ACTION_CANCEL);
-						}
-						return handled;
+			float x = Math.abs(velocityX);
+			if (x > Math.abs(velocityY) && x > activity.getResources().getDisplayMetrics().widthPixels) {
+				if (Math.abs(e1.getX()-e2.getX()) > Math.abs(e1.getY()-e2.getY())) {
+					handled = (velocityX > 0 ? onFlickRight() : onFlickLeft());
+					if (handled) {
+						e2.setAction(MotionEvent.ACTION_CANCEL);
 					}
+					return handled;
 				}
 			}
 			return false;
